fcb1bc815752bfa3b08deaff0645b1d57cd73f33
JORTEGA\Student <j_ortega34@yahoo.com>
2018-10-18 15:22:40 -0500

    Created ProductFilter

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts; //added
 using MyShop.Core.Models; // added
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,29 @@ namespace MyShop.WebUI.Controllers
             productCategories = productCategoryContext;
         }   // up to here
 
-        public ActionResult Index()
+        public ActionResult Index(string Category=null) //updated action for our ProductListViewModel
         {
-            List<Product> products = context.Collection().ToList(); //gets a list of products and sends it to the view
-            return View(products);
+            //List<Product> products = context.Collection().ToList(); //gets a list of products and sends it to the view
+            //changed after ProductListViewModel
+
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products = context.Collection().ToList(); //if Category is null our list of products will be all products
+            }
+            else
+            {
+                products = context.Collection().ToList().Where(p => p.Category == Category).ToList();
+                // if not null it will return a list of filtered products using this SQL statement
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model); //replaced products because we want it to return the model now
         }
 
         public ActionResult Details(string Id)
