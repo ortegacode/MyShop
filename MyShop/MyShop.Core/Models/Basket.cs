c260781a83f731fd380b0a3186f0a8482cfd8c5b
JORTEGA\Student <j_ortega34@yahoo.com>
2018-10-24 15:12:24 -0500

    Added Basket Model

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        // setting it to virtual ICollection EF will know that every time we load Basket from the db it will also load all the basket items. AKA (Lazy Loading)
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>(); // constructor that creates an empty list of basket items on creation
+        }
+    }
+}
---
+++ MyShop/MyShop.Core/Models/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketId { get; set; } // link back to the basket that contains the basket items
+        public string ProductId { get; set; }  // link to product
+        public int Quantity { get; set; }
+    }
+}
